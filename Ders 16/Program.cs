f9a5fa3fc68b42c0bbd88268e424657449638951
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-24 02:07:08 +0300

    Add project files.

---
+++ Ders 16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+        
+            Console.ReadLine();
+        }
+    }
+}
